04c7a93d9d689e9232a384d2f6ede56a15f743e9
JADSADAPORN BOONCHOM <s6302041610014@kmutnb.ac.th>
2021-01-12 15:14:27 +0700

    first commit

---
+++ c programing/addition.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+// ตั้ง library input output 
+void main(){
+    // ส่วนที่ 2 function หลักของภาษา c คือ fx main 
+
+    // ---------- คำสั่งโปรแกรมบวกเลข ----------
+    int n1,n2,total;
+    // ประกาศตัวแปร n1 n1 n3 and total 
+
+    printf("value of n1 is :");
+    scanf("%d",&n1);
+
+    printf("value of n2 is :");
+    scanf("%d",&n1);
+
+    total = n1 + n2;
+    printf("result is %d",total);
+
+
+
+    // ---------- คำสั่งโปรแกรมบวกเลข ----------
+
+}
+
---
+++ c programing/helloworld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// ส่วนหัวหลักดึง library (1)
+// การกด ctrl + / คือการใส่คอมเม้นต์แบบเร็ว short cut key
+
+// ส่วน function หลัก (2)
+void main()
+{
+    printf("Ryu Takasu\nRyu\n20\nGO GO BAR\nBEER\nWith my party");
+
+    /* ส่วนคำสั่งการทำงาน (3)
+   printf คือ ฟังก์ชั่นที่ใว้สำหรับแสดงผลทางหน้าจอ
+    */
+    //    วิธีการจัดชุดคำสั่งให้สวย ให้ shift + alt + f
+    // การเปิด terminal ให้กด ctrl + j
+}
